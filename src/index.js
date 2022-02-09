771f84ba889210b7076b9e060cc87f6c840b987c
Zach Shallbetter <zach@cursedcrypt.com>
2022-02-09 05:59:36 -0800

    chromatic test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import App from "pages/App";
+import reportWebVitals from "utils/reportWebVitals";
+const $ = window.$;
 
 ReactDOM.render(
   <React.StrictMode>
